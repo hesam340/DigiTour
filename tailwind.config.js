21a039b9edc3dd4a5b8ef9ec8ce6e420a767e5c4
hesam khaki <hesamkhaki340@gmail.com>
2025-07-31 19:43:47 +0330

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        yekan: [`var(--font-yekan)`],
+        vazir: [`var(--font-vazir)`],
+      },
+      colors: {
+        primary: "#FDB713",
+        secondary: "#10411B",
+        complementry: "#009ECA",
+        textColor: "#282828",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
